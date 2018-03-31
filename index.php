72f5d3f1bb8f41aafb03d982ce95bb041c6e987b
uires <uiresdeivide1@gmail.com>
2018-03-31 14:38:49 -0300

    Alteração na lógica de seleção

--- index.php
+++ index.php
@@ -12,48 +12,44 @@
 			echo "error pdo ->" . $e->getMessage();
 			exit;
 		}?>
-		<form >
-			<select name="ordenar"method="GET" onchange="this.form.submit()" >
+		<form method="POST">
+			<select name="ordenar" onchange="this.form.submit()" >
 				<option></option>
 				<option value="orderByNome">order by nome</option>
 				<option value="orderByIdade">order by idade</option>
 			</select>
 		</form>
-		<?php
-		if(isset($_GET['ordenar']) AND !empty($_GET['ordenar']) == false){
-			$ordem = addslashes($_GET['ordenar']);
-			if($ordem == "orderByNome") {
-				$sql = "SELECT * FROM usuarios ORDER BY nome ASC";
-			}
-			if($ordem == "orderByIdade"){
-				$sql = "SELECT * FROM usuarios ORDER BY idade ASC";
-			}else{
-
-			}
-		}else{
-			$sql = "SELECT * FROM usuarios";
-		}
-		?>
 		<table border="0" width="400" style="margin: auto;">
-			
 			<tr>
 				<th>Nome:</th>
 				<th>Idade:</th>
 			</tr>
-
 			<?php
-			
-			$sql=$pdo->query($sql);
-
+			/*
+				Por padrão o query é apenas uma seleção do bando de dados
+				mas caso haja um escolha do option, há verificação do qual
+				ordenação foi  escolhida.		
+			*/
+			$sql = "SELECT * FROM usuarios";
+			if(isset($_POST['ordenar']) AND empty($_POST['ordenar']) == false){
+				if($_POST['ordenar'] == "orderByNome"){
+					$sql = "SELECT * FROM usuarios ORDER BY nome ASC";
+				}
+				if($_POST['ordenar'] == "orderByIdade"){
+					$sql = "SELECT * FROM usuarios ORDER BY idade ASC";
+				}
+			}			
+			$sql= $pdo->query($sql);
 			if($sql->rowCount() > 0){
-				foreach ($sql->fetchAll() as $usuario):?>
+				foreach ($sql->fetchAll() as $usuario){?>
 					<tr>
 						<td><?php echo $usuario['nome'] ?></td>
 						<td><?php echo $usuario['idade'] ?></td>
 					</tr>
 				<?php
-				endforeach;
-			}?>
+				}
+			}
+			?>
 
 		</table>
 	</body>
